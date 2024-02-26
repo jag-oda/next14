27854b6d0b920aa8224d1a74d86e6387db024fc8
jnatorska <jnatorska@novomatic-tech.com>
2024-02-26 11:52:30 +0100

    graphql - part 1

--- src/api/products.ts
+++ src/api/products.ts
@@ -15,11 +15,49 @@ type ProductType = {
 };
 
 export const getProductsList = async () => {
-    const res = await fetch("https://naszsklep-api.vercel.app/api/products?take=20");
-	const productsResponse = (await res.json()) as ProductType[];
-    const products = productsResponse.map((product) : ProductItemType => productResponseItemTypeToProductItemType(product));
+    const res = await fetch("https://graphql.hyperfunctor.com/graphql", {
+        method: "POST",
+        body: JSON.stringify({
+            query: /* GraphQL */ `
+                query GetProductsList {
+                    products(first: 10) {
+                        id
+                        name
+                        description
+                        images {
+                            url
+                        }
+                        price
+                    }
+                }
+                `
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
 
-  return products;
+    const graphqlResponse = await res.json()
+  //  const res = await fetch("https://naszsklep-api.vercel.app/api/products?take=20");
+	//const productsResponse = (await res.json()) as ProductType[];
+  //  const products = productsResponse.map((product) : ProductItemType => productResponseItemTypeToProductItemType(product));
+  if(graphqlResponse.errors){
+    throw TypeError(graphqlResponse.errors[0].message)
+  };
+  console.log('graph ql response', graphqlResponse.data.products)
+  return  graphqlResponse.data.products.map(p => {
+    return {
+        id: p.id,
+        title: p.name,
+        price: p.price,
+        description: p.descriptions,
+        category: "",
+        coverImage: {
+            src: p.image[0].url,
+            alt: p.name
+        }
+    };
+  })    
 }
 
 export const getProductById =  async (id: ProductType["id"]) => {
